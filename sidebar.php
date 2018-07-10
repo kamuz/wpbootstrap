0da7654fd702b2c2c482c77b4c0080fd168eed25
Nick Bosswell <nick@inklessideas.co.uk>
2018-07-10 09:37:26 +0300

    Add search form

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,18 @@
 <div class="col-lg-3">
-            <?php if(is_active_sidebar('categories')): ?>
-            <?php dynamic_sidebar('categories') ?>
-            <?php endif; ?>
-            <?php if(is_active_sidebar('sidebar')): ?>
-            <?php dynamic_sidebar('sidebar') ?>
-            <?php endif; ?>
+    <div class="well">
+    <form role="search" method="get" action="<?php esc_url(home_url('/')) ?>">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="s">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
         </div>
+    </form>
+    </div>
+    <?php if(is_active_sidebar('categories')): ?>
+    <?php dynamic_sidebar('categories') ?>
+    <?php endif; ?>
+    <?php if(is_active_sidebar('sidebar')): ?>
+    <?php dynamic_sidebar('sidebar') ?>
+    <?php endif; ?>
+</div>
